c8859cbfff272baa7a5bca1dc321163988df5684
Boggy Jang <boggyjan@gmail.com>
2022-05-27 12:23:34 +0800

    update nuxt ver

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
     '@/assets/css/main.scss'
   ],
+
   typescript: {
     strict: true
   },
