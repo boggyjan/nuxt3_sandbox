0add4becfe56dffe94264e5be02cc846dfcec8d8
Boggy Jang <boggyjan@gmail.com>
2023-08-17 16:37:34 +0800

    add hydration fails page

---
+++ pages/render_error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Test Render Error
+    <hr>
+    <div v-if="isMonday">
+      isMonday: {{ isMonday }}
+      <hr>
+    </div>
+
+    <button
+      type="button"
+      class="btn"
+      @click="doSomething()"
+    >
+      doSomething()
+    </button>
+  </div>
+</template>
+
+<script setup>
+const isMonday = process.server
+  ? new Date(new Date().getTime() - 18 * 60 * 60 * 1000).getDay() === 1
+  : new Date().getDay() === 1
+
+console.log(isMonday)
+
+function doSomething () {
+  console.log('doSomething()')
+}
+</script>
---
+++ pages/render_error_option_api.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    Test Render Error
+    <hr>
+    <div v-if="isMonday">
+      isMonday: {{ isMonday }}
+      <hr>
+    </div>
+
+    <button
+      type="button"
+      class="btn"
+      @click="doSomething()"
+    >
+      doSomething()
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    const isMonday = process.server
+      ? new Date(new Date().getTime() - 18 * 60 * 60 * 1000).getDay() === 1
+      : new Date().getDay() === 1
+
+    console.log(isMonday)
+
+    return {
+      isMonday
+    }
+  },
+
+  methods: {
+    doSomething () {
+      console.log('doSomething()')
+    }
+  }
+}
+</script>
